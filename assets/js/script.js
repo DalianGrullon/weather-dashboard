118dbc9ee08db464c5d53970ad7442f3576806a0
Dalian Grullon <dalian.business@aol.com>
2022-07-06 02:22:23 -0400

    added form in aside element and initial js functionality structure

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// SEARCH //
+// grabbing the values from the search, to later use in our functions 
+function searchHandle () {
+    // grab the search content
+    // fetchLocation(data we received from the search)
+  }
+  
+  // FUNCTIONALITY FOR FETCHING LOCATION //
+  // activity 3 - mod 6
+  // call the location api
+   
+  function fetchLocation (search) {
+    // get some data pertaining to location - using an api call
+  
+    // fetchWeather(data we received from fetchLocation)
+  }
+  
+  // FUNCTIONALITY FOR FETCHING WEATHER //
+  
+  function fetchWeather (location) {
+    // get some data for the weather in the location we searched - using an api call
+  
+    renderWeather(data)
+  }
+  
+  
+  // FUNCTIONALITY FOR RENDERING CONTENT //
+  
+  function renderWeather(data) {
+    // render the weather 
+  }
+  
+  // HISTORY FEATURE //
+  // local storage 
